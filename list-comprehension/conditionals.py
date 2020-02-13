17ccfe1ca84fe78382185954bbd8f5e263a17dc9
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-13 06:57:00 -0500

    :sparkles: List comprehension pt1

---
+++ list-comprehension/conditionals.py
@@ -0,0 +1,7 @@
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry',
+              'aragorn', 'legolas', 'boromir', 'gimli']
+
+new_fellowship = [member for member in fellowship if len(member) >= 7]
+
+print(new_fellowship)
