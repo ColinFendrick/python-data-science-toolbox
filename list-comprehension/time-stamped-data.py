31b489c18a9dda660738dfda26c8c96dc24f72e3
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-14 08:44:09 -0500

    :sparkles: List comprehension pt3

---
+++ list-comprehension/time-stamped-data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('../tweets.csv')
+
+# Extract the created_at column from df: tweet_time
+tweet_time = df['created_at']
+
+# Extract the clock time: tweet_clock_time
+tweet_clock_time = [entry[11:19] for entry in tweet_time]
+
+print(tweet_clock_time)
